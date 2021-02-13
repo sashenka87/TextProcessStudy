223ed67cdbd4dd9b35b2329e6c25969caecc221e
sashenka87 <sashenka87@gmail.com>
2021-02-13 00:17:40 -0500

    Update step2.vue

--- app/javascript/components/tutorial/panes/step2.vue
+++ app/javascript/components/tutorial/panes/step2.vue
@@ -8,7 +8,7 @@
       You will also be able to click on <b>hyperlinks</b> to learn about references in text.
     </p>
     
-    <img src="https://lh3.googleusercontent.com/-_WD9zsDclwixnVrDUujEpbyjbUi1kS2ur98JGElZexYR0yDNg71_nx5w_btFPnavSwENb_QmKVVSLuVILw6Hc92t1mZ7SzvuI6VicDMPiQmJ34YXAU9eWEGc7BHvg8YVzJY1Ow89uOc7DPqLVeuOY907VePy6a16j_wvWJ7cxk5g4RAsb0rdvAds4opFx5sRqedIOXApcQR7Z9V74HL3f_STkWbKfFu1_s74y9sZ6muHcClMmYSPkGFFzmjd51ZjwO05hutxiwzxkHOCGVRB0BcfNMStfhErPdH42D87y4YTAHkH57U09Ro3MC_hbWDgkEJWyupt3XLQ0aPgCW4OvWWQbZ5rAY01ZywjsFaOREyYwf8Uq87py4zLXiCypDXbJApU-UfAGq_qYenUmGwzONSbpudAO07QHs8Dh_ajNkVYcArWcRx3RJLo88m8q1ByFIRaC1d6b8PEFA-BaBKDERvR-a9e9WVrhdd0dBcVqXH4F0IpM1PsiLtDp3HE0symfx9Y-rz_Bum8lKjzAtjLYJm8cvmk3Py6VtL_RfaoYCBp25Th_sQnRcZ9UZkiul6BUDlDme8NX8ZVv6P8UPfsANdei6HGoH3NIZDGlgdsqW8NxILpI47Y6si4-IeapndgKqQjhDjclnkwAjiY6aoKf3yZEArf93uiVYEvu6TfKuS3Jeq_1PeanGq1GkP=w2876-h706-no?authuser=0" width="1000">
+    <img src="https://lh3.googleusercontent.com/pw/ACtC-3eTrZEwjENuq4F7Zr0vPPQAJGnaH7o1uzm_C6WpLjcY4w4nomJw273xgNsElNysXQ5fFR1GVL9Ju3Jeaf6lxKdf2xWgSuJCym5bbf0nIuHwew0n9kZvZlyg3_K58RND4loxun-Z_fKnAgUnz5UeV4rQ=w2876-h706-no?authuser=0" width="1000">
 
 
     <next-pane></next-pane>
