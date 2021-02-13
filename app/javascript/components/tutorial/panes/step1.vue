751fba9b79dc76b255ad0ee7fa433a20b047c47c
sashenka87 <sashenka87@gmail.com>
2021-02-12 22:14:43 -0500

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -12,6 +12,8 @@
       You will have two highlighter colors available.
       You can highlight by selecting any portion text and clicking the green or yellow highlighter.
     </p>
+    
+    <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQ-iI5x3CTI2uPKpMJqAlapiJd8cJxcF7XmTRId2_hweVlmRqkdngEL1mdR61qUwld53LAgR2_WXQX_/embed?start=false&loop=false&delayms=3000" frameborder="0" width="1440" height="839" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
 
     <next-pane></next-pane>
   </div>
