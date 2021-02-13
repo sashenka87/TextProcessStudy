b868b6d625d50b1dece9193d676a0127e62ca6a6
sashenka87 <sashenka87@gmail.com>
2021-02-13 00:20:53 -0500

    Update step2.vue

--- app/javascript/components/tutorial/panes/step2.vue
+++ app/javascript/components/tutorial/panes/step2.vue
@@ -8,7 +8,7 @@
       You will also be able to click on <b>hyperlinks</b> to learn about references in text.
     </p>
     
-    <img src="https://lh3.googleusercontent.com/pw/ACtC-3eTrZEwjENuq4F7Zr0vPPQAJGnaH7o1uzm_C6WpLjcY4w4nomJw273xgNsElNysXQ5fFR1GVL9Ju3Jeaf6lxKdf2xWgSuJCym5bbf0nIuHwew0n9kZvZlyg3_K58RND4loxun-Z_fKnAgUnz5UeV4rQ=w2876-h706-no?authuser=0" width="1000">
+    <img src="https://lh3.googleusercontent.com/pw/ACtC-3eTrZEwjENuq4F7Zr0vPPQAJGnaH7o1uzm_C6WpLjcY4w4nomJw273xgNsElNysXQ5fFR1GVL9Ju3Jeaf6lxKdf2xWgSuJCym5bbf0nIuHwew0n9kZvZlyg3_K58RND4loxun-Z_fKnAgUnz5UeV4rQ=w2876-h706-no" width="1000">
 
 
     <next-pane></next-pane>
