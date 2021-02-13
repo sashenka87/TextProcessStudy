7c14f223a3646710917edbaf2b3f86f3774f48b3
sashenka87 <sashenka87@gmail.com>
2021-02-12 22:41:44 -0500

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -12,7 +12,7 @@
       You will have two highlighter colors available.
       You can highlight by selecting any portion text and clicking the green or yellow highlighter.
     
-<img src="https://lh3.googleusercontent.com/pw/ACtC-3egUfdQSCQb135TSaX00clqHDVFodb9jY2kBuR7ylfpoVYYZS7cuS0PKscLBpaiGTiRNBEa-Pgw0zT4Wmmv6MXrnQJZjCbbm5Lyd-AjDgZlOIpdPaPiVpktITu3WxpNLVOUwcKi1ay7Oj-vwZGKWOFr=w2836-h636-no?authuser=0" width="100">
+<img src="https://lh3.googleusercontent.com/k683Tum9oOvZlY0QUC8VjYpFHzVbLKwTZYsChbwzFUeuxpq94c-sLfC9zaalloyTUN8Og7Fm3_koXeuG-Nl6AVHzWfSijuSK_MBAm3lwN2GG8MUsoLnT2TYMyhdQFItjaIZhVnmPi-lt87fav7yEkV8kW95BrmXaHT2gu3bozlfVRsz6jn9_z-auc1PeJB1Fk8DAW1vj4AoaZbAyRKkr3T4jmogWdUIjile833GXRUadkFmM4buQMYYyNES5yXvxOiBii0YHgHmN6a78SdINRQwVeDc092w5H2xhNmWugo4AlEm_qxMXQxj3mwSH1ZjEFuZm2nRLfnf4nqQtwLoBasI7QPuURnFkjsii8jvwSBqUbFYVK3Xee4_R4IL0yfpZz43LaD6_ywAUscJ3xRcqHP-jKUBJs3kDgskfV0TZ9ooDyv_Gg7u4OikCvIE29-tfFVMamwjhDru3a5bzTAl1ivEpBahKkjyJ_69-jTuVDpnxyyDLpQqtriAJpiERP2slUyIZkXxF1VlntmutyY_oYpwOANQpQVmOTUui8EC82AvznDrptaDhyXsNVcr5e8UmXMMjii2xgHEWXyjaFW0EMPlheJufpGoedgXy6W1F-Oo9A8Z_Vr1VDW-nnOBmgRybXf0OaeDUXOOKylXKrw6jFanhPZ3RTE33YhmMaWitUTyltygrRw4X5QxhoBus=w2876-h646-no?authuser=0" width="100">
     </p>
     <br>
     <next-pane></next-pane>
