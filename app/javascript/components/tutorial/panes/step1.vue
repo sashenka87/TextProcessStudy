e379d3c135e893194d0736b2c71d347112f15a54
sashenka87 <sashenka87@gmail.com>
2021-02-13 00:16:48 -0500

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -13,7 +13,7 @@
       You can highlight by selecting any portion text and clicking the green or yellow highlighter.
        </p>
        <br>
-<img src="https://lh3.googleusercontent.com/xwSaKNmxlrtcInGTIofcA3Rj40pPgiR-3ZmdA0MpmUii6D1hr8reB35EdS8QCzQEAmMt8LGjFnyEkzRHaslKrKb1KIklABX7srdowgUaoJg-Jskv69oKKGjXqxEFMI6w6lhXhOtrCA2AfcOwbF-XhpIvccCy9K6-lvVCMuggGlY5D3gaHL1iYOL5I9cQLuEn773oyRJglBC7HejsLBdlDkndV12LkEGrzR4O-_VpZ-xvqKBN3wa_oRuapXCgg7Tq_CI8Lz2e8mIYm_e-CFxOhDNhoSNN312lk_KiZwlMbZq5VueKIWFFQWW3Ss-csM5huUmUz2lZqKCAdv7V90ZP1l1UVij_lb7ZxShspOSwmcosHXVX44egOcLGqLVTRjuiJ2NKQjeUuGYoKWG3NAlRGwkYzP3rboKuOrObSPs1KwmPld7yzzSHm934MiK4DwX-O3xFNojjWQSPlGOSTH4yMVKvs36cCW-z_z3rZF5m-rL4QVzciUGpM-mJwPN9C1EOjPoU4UZnRQQcr1oxYrcqVQitcyt45DKrLT2uziyJQ0nA0LXogZrk4D8HIMLWPtKIi2wPaoAooXNaic1zHDt5GJ2huV0z2jVhc1Sgst8PaQpghpDJC4JuZaXFITL8WSbUJTziawoFlK5ZkPppUcUwY2tYZzXLOvBQm9FCjTplTS8ohW3PAL8yX8OoWjGx=w2876-h642-no?authuser=0" width="1000">
+<img src="https://lh3.googleusercontent.com/pw/ACtC-3cOAFSgDn3YATILAzQjfuEbr_5gzHtRcorEm4zgm34dhOnLZcl_YUVwJvSR2F852jBXDI5TWpwAuvVVguPGhUqFffn97BIc62-ohpFaC8zDsHViB2r69CUoA-Z8K7--hysK9nZnuTvUpyR1oFm9JByG=w2876-h646-no?authuser=0" width="1000">
  
     <br>
     <next-pane></next-pane>
