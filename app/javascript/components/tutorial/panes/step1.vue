67c393cb9f005a6fd271bbf865fa0f750f6b8b28
sashenka87 <sashenka87@gmail.com>
2021-02-24 20:40:00 -0500

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="box content">
     <h3 class="is-size-5 has-text-weight-semibold">
-      Highlighter and Comment Tool
+      Highlight and Comment on any Information Related Across Texts
     </h3>
 
     <p>
-      As you read, you will be able to highlight and make comments about any <b>related information presented across texts</b>.
+      As you read, you will be able to highlight any <b>relations</b> or <b>connections across texts</b>.
     </p>
 
     <p>
       You will have two highlighter colors available.
       You can highlight by selecting any portion text and clicking the green or yellow highlighter.
        </p>
+       <p>
+        You will also be able to add <b>comments</b> to explain any relations or connections across texts that you highlighted.
+        </p>
        <br>
 <img src="https://lh3.googleusercontent.com/P_yJ66iXCEz1ZdDxlYLVgbJuXuQwUltBt-kVePsIZpZYK5ZlevMyLZsfDUgwZU8fCVjDqIUnVyKp2GZGkS7T3ml455QnOv0uYlwSyohMiSs1CagiSnDo9ZSziDX6gtaGR6BWsWmJxmH8vdzusNXOuukyba8fvU738QG51C1Hew5UIsTuPkxvOc6efkfzGLAHLFL4N0NOrSPwcRA4jQfDfapycDUfdSeGLRBz4CVzd5ra8LZlCZQ38KyMwYOxdLVjdBj_zpoRbwLKMUm1rAvGpQy6zxu6TK_ILt7g-Lz7EX0iF3cSHXa3BwQDweQcuFu8IBOfgPA0oNZqLT9pAHPj69JaCQ4Bf86KMSxYIct_jUCsQGQp3r0quHOm9z2GP4LILkrXJ2CI3YojDyzOua0tYpbdnoIX_34xVu44sTQcwEQlVj7-q0f4DHH1UeslPEO_fHQXa5sCTh17O_rEZ4xUJhhwK927OIGdnGG5NmvvKZK2xv9P77Ejg4CG4SabPgN8YheR54QqXfH8Mw0-tX50UiE-1eDbNpbDEmY9CqumApjjqkaQoEoA-i8s_6hBeSW9wLQjnolJadbaWv5oFGtfCijfoEGupWr4I9gGMafMWSX2Wj6et4Mw78FZSTezeM2RxTkRoqC3Dl0mNRzWzDMKUhuAYtI3F7HvydarOZfZ2LrnG7VuRcDkHYBymfa9=w3272-h820-no?authuser=0" width="1000">
  
