df3e657492326bd250f066d00643ec6f67568492
sashenka87 <sashenka87@gmail.com>
2021-02-24 20:33:44 -0500

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -10,10 +10,10 @@
 
     <p>
       You will have two highlighter colors available.
-      You can highlight by selecting any portion text and clicking the green or yellow highlighter.
+      You can highlight by selecting any portion of text and clicking the green or yellow highlighters.
        </p>
           <p>
-      You will also be able to add comments about any highlighted text.
+      You will also be able to add comments to any highlighted text.
        </p>
        <br>
 <img src="https://lh3.googleusercontent.com/P_yJ66iXCEz1ZdDxlYLVgbJuXuQwUltBt-kVePsIZpZYK5ZlevMyLZsfDUgwZU8fCVjDqIUnVyKp2GZGkS7T3ml455QnOv0uYlwSyohMiSs1CagiSnDo9ZSziDX6gtaGR6BWsWmJxmH8vdzusNXOuukyba8fvU738QG51C1Hew5UIsTuPkxvOc6efkfzGLAHLFL4N0NOrSPwcRA4jQfDfapycDUfdSeGLRBz4CVzd5ra8LZlCZQ38KyMwYOxdLVjdBj_zpoRbwLKMUm1rAvGpQy6zxu6TK_ILt7g-Lz7EX0iF3cSHXa3BwQDweQcuFu8IBOfgPA0oNZqLT9pAHPj69JaCQ4Bf86KMSxYIct_jUCsQGQp3r0quHOm9z2GP4LILkrXJ2CI3YojDyzOua0tYpbdnoIX_34xVu44sTQcwEQlVj7-q0f4DHH1UeslPEO_fHQXa5sCTh17O_rEZ4xUJhhwK927OIGdnGG5NmvvKZK2xv9P77Ejg4CG4SabPgN8YheR54QqXfH8Mw0-tX50UiE-1eDbNpbDEmY9CqumApjjqkaQoEoA-i8s_6hBeSW9wLQjnolJadbaWv5oFGtfCijfoEGupWr4I9gGMafMWSX2Wj6et4Mw78FZSTezeM2RxTkRoqC3Dl0mNRzWzDMKUhuAYtI3F7HvydarOZfZ2LrnG7VuRcDkHYBymfa9=w3272-h820-no?authuser=0" width="1000">
