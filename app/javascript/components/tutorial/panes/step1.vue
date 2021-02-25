2071a7368c654632e2d7d312cf1df0730f217164
sashenka87 <sashenka87@gmail.com>
2021-02-24 20:20:23 -0500

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -16,7 +16,7 @@
       You will also be able to add comments about any highlighted text.
        </p>
        <br>
-<img src="https://photos.app.goo.gl/BmTeWA3iUMj7EtDq6" width="1000">
+<img src="https://lh3.googleusercontent.com/P_yJ66iXCEz1ZdDxlYLVgbJuXuQwUltBt-kVePsIZpZYK5ZlevMyLZsfDUgwZU8fCVjDqIUnVyKp2GZGkS7T3ml455QnOv0uYlwSyohMiSs1CagiSnDo9ZSziDX6gtaGR6BWsWmJxmH8vdzusNXOuukyba8fvU738QG51C1Hew5UIsTuPkxvOc6efkfzGLAHLFL4N0NOrSPwcRA4jQfDfapycDUfdSeGLRBz4CVzd5ra8LZlCZQ38KyMwYOxdLVjdBj_zpoRbwLKMUm1rAvGpQy6zxu6TK_ILt7g-Lz7EX0iF3cSHXa3BwQDweQcuFu8IBOfgPA0oNZqLT9pAHPj69JaCQ4Bf86KMSxYIct_jUCsQGQp3r0quHOm9z2GP4LILkrXJ2CI3YojDyzOua0tYpbdnoIX_34xVu44sTQcwEQlVj7-q0f4DHH1UeslPEO_fHQXa5sCTh17O_rEZ4xUJhhwK927OIGdnGG5NmvvKZK2xv9P77Ejg4CG4SabPgN8YheR54QqXfH8Mw0-tX50UiE-1eDbNpbDEmY9CqumApjjqkaQoEoA-i8s_6hBeSW9wLQjnolJadbaWv5oFGtfCijfoEGupWr4I9gGMafMWSX2Wj6et4Mw78FZSTezeM2RxTkRoqC3Dl0mNRzWzDMKUhuAYtI3F7HvydarOZfZ2LrnG7VuRcDkHYBymfa9=w3272-h820-no?authuser=0" width="1000">
  
     <br>
     <br>
