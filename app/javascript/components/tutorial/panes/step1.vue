491cb1c011a3387df3313859c2263394599cc7d8
sashenka87 <sashenka87@gmail.com>
2021-02-24 20:41:35 -0500

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="box content">
     <h3 class="is-size-5 has-text-weight-semibold">
-      Highlight and Comment on any Information Related Across Texts
+      Highlight and Comment on Any Relations or Connections Across Texts
     </h3>
 
     <p>
