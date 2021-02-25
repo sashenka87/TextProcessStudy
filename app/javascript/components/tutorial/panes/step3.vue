193e9d40c5426af4cd712da96a4f7168596ef723
sashenka87 <sashenka87@gmail.com>
2021-02-24 20:54:28 -0500

    Update step3.vue

--- app/javascript/components/tutorial/panes/step3.vue
+++ app/javascript/components/tutorial/panes/step3.vue
@@ -1,22 +1,17 @@
 <template>
   <div class="box content">
     <h3 class="is-size-5 has-text-weight-semibold">
-    Please pay attention to <u>important information</u> as you read.
+    Ready?
     </h3>
 
     <p>
-      Ready? On the next page you’ll see the first text.
+      On the next page you'll see the <b>first<b/> of <b>four</b> texts you will be asked to read to write a research report on mass incarceration in the United States. 
     </p>
 
     <p>
-      Please read each text to write a research report on mass incarceration in the United States. 
+      Please be sure to highlight and comment on any <b>related</b> or <b>connected information across texts</b>, as you read.
     </p>
-
-    <p>
-      Please be sure to highlight and comment on <b>important information</b> as you read.
-      You will be able to click on hyperlinks to learn more about the references in each text. 
-    </p>
-
+    
     <div class="has-text-right">
       <a href="/reading" class="button is-success">Take me to my reading &rarr;</a>
     </div>
