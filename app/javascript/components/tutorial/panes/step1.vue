7612401b1d3bd72236bfa42773ac80f20a372a9d
sashenka87 <sashenka87@gmail.com>
2021-02-27 15:48:43 -0500

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -11,12 +11,9 @@
     <p>
       <img src="https://i.postimg.cc/YSQCHSL0/Screen-Shot-2021-02-27-at-3-34-22-PM.png" width="30">
       You can highlight by selecting any portion of text and then <br>
-      
-    <img src="https://i.postimg.cc/YSQCHSL0/Screen-Shot-2021-02-27-at-3-34-22-PM.png" width="30"> clicking one the yellow or green highlight buttons.
-
-       </p>
-          <p>
-     <img src="https://i.postimg.cc/YqVrLF1j/Screen-Shot-2021-02-27-at-3-34-45-PM.png" width="30"> You will also be able to add <b>comments</b> to explain any important information you highlighted.
+   
+    <img src="https://i.postimg.cc/y6LYBtxm/Screen-Shot-2021-02-27-at-3-34-32-PM.png" width="30"> clicking one the yellow or green highlight buttons. <br>
+     <img src="https://i.postimg.cc/YqVrLF1j/Screen-Shot-2021-02-27-at-3-34-45-PM.png" width="30"> You can then add <b>comments</b> to explain any important information you highlighted.
        </p>
        <br>
 <img src="https://i.postimg.cc/vmNRrJ27/Figure-1-Screenshot.png" width="1000">
