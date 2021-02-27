f2ef9b6149b221286334885d0b1bc0b89b00473e
sashenka87 <sashenka87@gmail.com>
2021-02-27 17:20:46 -0500

    Update step2.vue

--- app/javascript/components/tutorial/panes/step2.vue
+++ app/javascript/components/tutorial/panes/step2.vue
@@ -5,7 +5,7 @@
     </h3>
 
     <p>
-     <img src="https://i.postimg.cc/8k9j8xj7/Screen-Shot-2021-02-27-at-5-15-11-PM.png" width="30"> You will also be able to click on <b>hyperlinks</b> to learn about references in text.
+     <img src="https://i.postimg.cc/8k9j8xj7/Screen-Shot-2021-02-27-at-5-15-11-PM.png" width="40"> You will also be able to click on <b>hyperlinks</b> to learn about references in text.
     </p>
         
     <img src="https://i.postimg.cc/dtZZLwzR/Screen-Shot-2021-02-27-at-5-13-43-PM.png" width="1000">
