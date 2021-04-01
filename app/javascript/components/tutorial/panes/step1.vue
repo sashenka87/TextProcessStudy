6f72a1336b4c117f56821dc42ee2e05131cd3a2c
sashenka87 <sashenka87@gmail.com>
2021-04-01 14:20:30 -0400

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="box content">
     <h3 class="is-size-5 has-text-weight-semibold">
-      Highlight and comment on any <u>important information</u> you read
+      Highlight and comment on information during reading
     </h3>
 
     <p>
-      As you read, please highlight and comment on any <b>important information</b> in texts.
+     You will be able to <b>higlight</b> and <b>comment</b> on information during reading.
     </p>
 
     <p>
@@ -13,7 +13,7 @@
       You can highlight by selecting any portion of text and <br>
    
     <img src="https://i.postimg.cc/y6LYBtxm/Screen-Shot-2021-02-27-at-3-34-32-PM.png" width="30"> Clicking the yellow or green highlight buttons. <br>
-     <img src="https://i.postimg.cc/YqVrLF1j/Screen-Shot-2021-02-27-at-3-34-45-PM.png" width="30"> You can then add <b>comments</b> to explain any important information you highlighted.
+     <img src="https://i.postimg.cc/YqVrLF1j/Screen-Shot-2021-02-27-at-3-34-45-PM.png" width="30"> You can then add <b>comments</b> to explain any information you highlighted.
        </p>
        <br>
 <img src="https://i.postimg.cc/vmNRrJ27/Figure-1-Screenshot.png" width="1000">
