82ad2ded06f5a8af8c2e558fec6ae385552cf327
sashenka87 <sashenka87@gmail.com>
2021-04-01 14:25:58 -0400

    Update step2.vue

--- app/javascript/components/tutorial/panes/step2.vue
+++ app/javascript/components/tutorial/panes/step2.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="box content">
     <h3 class="is-size-5 has-text-weight-semibold">
-      Hyperlinks
+      Hyperlinked Keywords
     </h3>
 
     <p>
-     <img src="https://i.postimg.cc/8k9j8xj7/Screen-Shot-2021-02-27-at-5-15-11-PM.png" width="30"> You will also be able to click on <b>hyperlinks</b> to learn about references in text.
+     <img src="https://i.postimg.cc/8k9j8xj7/Screen-Shot-2021-02-27-at-5-15-11-PM.png" width="30"> You will also be able to click on <b>hyperlinks</b> to learn about keywords in text.
     </p>
     
-    <img src="https://i.postimg.cc/dtZZLwzR/Screen-Shot-2021-02-27-at-5-13-43-PM.png" width="1000">
+    <img src="https://i.postimg.cc/N0FBFM5X/grab0.jpg" width="1000">
 
 <br>
     <next-pane></next-pane>
