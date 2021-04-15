24904d5646dd4f16f027c4cdeb866099ab3873ec
sashenka87 <sashenka87@gmail.com>
2021-04-15 16:50:49 -0400

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -3,7 +3,7 @@
     <h3 class="is-size-5 has-text-weight-semibold">
       Hyperlinked Keywords
     </h3>
-
+<br>
    <p>
      <img src="https://i.postimg.cc/YSQCHSL0/Screen-Shot-2021-02-27-at-3-34-22-PM.png" width="30"> You will be able to click on <b>hyperlinks</b> to learn about keywords in text.
     </p>
