8047d3d4f1d3abe1454f564afd7b457cb358e205
sashenka87 <sashenka87@gmail.com>
2021-04-15 18:03:59 -0400

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -1,25 +1,13 @@
 <template>
   <div class="box content">
     <h3 class="is-size-5 has-text-weight-semibold">
-      Highlight and comment on information during reading
+    Parenting Styles
     </h3>
 
     <p>
-     You will be able to <b>higlight</b> and <b>comment</b> on information during reading.
+    One of the textsbook excerpts will be about <b>parenting styles</b>.
     </p>
 
-    <p>
-      <img src="https://i.postimg.cc/YSQCHSL0/Screen-Shot-2021-02-27-at-3-34-22-PM.png" width="30">
-      You can highlight by selecting any portion of text and <br>
-   
-    <img src="https://i.postimg.cc/y6LYBtxm/Screen-Shot-2021-02-27-at-3-34-32-PM.png" width="30"> Clicking the yellow or green highlight buttons. <br>
-     <img src="https://i.postimg.cc/YqVrLF1j/Screen-Shot-2021-02-27-at-3-34-45-PM.png" width="30"> You can then add <b>comments</b> to explain any information you highlighted.
-       </p>
-       <br>
-<img src="https://i.postimg.cc/vmNRrJ27/Figure-1-Screenshot.png" width="1000">
-    <br>
-    <br>
-    <br>
     <next-pane></next-pane>
   </div>
 </template>
