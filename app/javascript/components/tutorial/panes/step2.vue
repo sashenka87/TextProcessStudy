b48870ec1f955a9ffb1c3ce59f920c53caa9228a
sashenka87 <sashenka87@gmail.com>
2021-04-15 14:49:20 -0400

    Update step2.vue

--- app/javascript/components/tutorial/panes/step2.vue
+++ app/javascript/components/tutorial/panes/step2.vue
@@ -1,12 +1,11 @@
 <template>
   <div class="box content">
     <h3 class="is-size-5 has-text-weight-semibold">
-      Hyperlinked Keywords
+      Comment on information during reading
     </h3>
 
-    <p>
-     <img src="https://i.postimg.cc/8k9j8xj7/Screen-Shot-2021-02-27-at-5-15-11-PM.png" width="30"> You will also be able to click on <b>hyperlinks</b> to learn about keywords in text.
-    </p>
+  <img src="https://i.postimg.cc/YqVrLF1j/Screen-Shot-2021-02-27-at-3-34-45-PM.png" width="30"> You will also be able to add <b>comments</b> to explain any information you highlighted.
+       </p>
     
     <img src="https://i.postimg.cc/N0FBFM5X/grab0.jpg" width="1000">
 
