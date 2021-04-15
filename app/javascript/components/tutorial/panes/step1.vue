a15efd70740b8e3cad4a112cc0704684439e9c8a
sashenka87 <sashenka87@gmail.com>
2021-04-15 14:54:00 -0400

    Update step1.vue

--- app/javascript/components/tutorial/panes/step1.vue
+++ app/javascript/components/tutorial/panes/step1.vue
@@ -1,22 +1,13 @@
 <template>
   <div class="box content">
     <h3 class="is-size-5 has-text-weight-semibold">
-      Highlight and comment on information during reading
+      Hyperlinked Keywords
     </h3>
 
-    <p>
-     You will be able to <b>higlight</b> and <b>comment</b> on information during reading.
+   <p>
+     <img src="https://i.postimg.cc/YSQCHSL0/Screen-Shot-2021-02-27-at-3-34-22-PM.png" width="30"> You will be able to click on <b>hyperlinks</b> to learn about keywords in text.
     </p>
 
-    <p>
-      <img src="https://i.postimg.cc/YSQCHSL0/Screen-Shot-2021-02-27-at-3-34-22-PM.png" width="30">
-      You can highlight by selecting any portion of text and <br>
-   
-    <img src="https://i.postimg.cc/y6LYBtxm/Screen-Shot-2021-02-27-at-3-34-32-PM.png" width="30"> Clicking the yellow or green highlight buttons. <br>
-     <img src="https://i.postimg.cc/YqVrLF1j/Screen-Shot-2021-02-27-at-3-34-45-PM.png" width="30"> You can then add <b>comments</b> to explain any information you highlighted.
-       </p>
-       <br>
-<img src="https://i.postimg.cc/vmNRrJ27/Figure-1-Screenshot.png" width="1000">
     <br>
     <br>
     <br>
