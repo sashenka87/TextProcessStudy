5882131fe42d7696ba2b3347f2de6043d8c77a1c
sashenka87 <sashenka87@gmail.com>
2021-04-15 14:55:05 -0400

    Update step2.vue

--- app/javascript/components/tutorial/panes/step2.vue
+++ app/javascript/components/tutorial/panes/step2.vue
@@ -8,7 +8,7 @@
      <img src="https://i.postimg.cc/8k9j8xj7/Screen-Shot-2021-02-27-at-5-15-11-PM.png" width="30"> You will also be able to click on <b>hyperlinks</b> to learn about keywords in text.
     </p>
     
-    <img src="https://i.postimg.cc/N0FBFM5X/grab0.jpg" width="1000">
+    <img src="https://i.postimg.cc/NMzC3HsT/grab36.jpg" width="1000">
 
 <br>
     <next-pane></next-pane>
