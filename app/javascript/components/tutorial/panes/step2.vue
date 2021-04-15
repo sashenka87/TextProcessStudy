1a70567e58701f255afc3761045719623904989e
sashenka87 <sashenka87@gmail.com>
2021-04-15 18:05:09 -0400

    Update step2.vue

--- app/javascript/components/tutorial/panes/step2.vue
+++ app/javascript/components/tutorial/panes/step2.vue
@@ -1,16 +1,12 @@
 <template>
   <div class="box content">
     <h3 class="is-size-5 has-text-weight-semibold">
-      Hyperlinked Keywords
+      Peer Social Statuses
     </h3>
 
-    <p>
-     <img src="https://i.postimg.cc/8k9j8xj7/Screen-Shot-2021-02-27-at-5-15-11-PM.png" width="30"> You will also be able to click on <b>hyperlinks</b> to learn about keywords in text.
-    </p>
-    
-    <img src="https://i.postimg.cc/N0FBFM5X/grab0.jpg" width="1000">
-
-<br>
+<p>
+    The other textbook excerpts will be about <b>peer social statuses</b>.
+</p>
     <next-pane></next-pane>
   </div>
 </template>
